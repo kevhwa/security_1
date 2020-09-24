c3c6b6314aa2c449b3e1ca99e50bde6e5cf6ccb6
Kevin Hwang <jch2169@columbia.edu>
2020-09-24 17:49:08 -0400

    added some more features and hash functionality

--- hw1/cstore.c
+++ hw1/cstore.c
@@ -5,9 +5,13 @@
 #include <stdio.h>
 #include <ctype.h> /* For isascii */
 #include <unistd.h>
+#include <sys/stat.h>
 // include others
 
 #define MAX_NAME_BUFF 100 /* Maximum buffer for names */
+#define MAX_PASS_BUFF 32
+#define MAX_DATA_BUFF 4096 /* Maximum buffer for transfering data */
+#define HASH_ITR 10000
 
 static void die(const char *message)
 {
@@ -18,25 +22,43 @@ static void die(const char *message)
 void checkPassValid(char *password)
 {
 	int length;
-	if((length = strlen(password)) > MAX_NAME_BUFF)
+	if((length = strlen(password)) > MAX_PASS_BUFF)
 		die("Password too long");
 	
-	for(int i = 0; i < length; i++)
-	{
+	for(int i = 0; i < length; i++) {
 		if(!isascii(password[i]))
 			die("ASCII characters only");
 	}
 }
 
+void addFile(FILE *newFile, FILE *archiveFile)
+{
+        size_t n;
+        char buf[MAX_DATA_BUFF];
+        
+        while ((n = fread(buf, 1, sizeof(buf), newFile)) > 0) {
+                
+                if ((fwrite(buf, 1, n, archiveFile)) != n) {
+                        die("write failed\n");
+                }
+        }
+
+        if (ferror(archiveFile)) {
+                die("fread failed\n");
+        } 
+
+        fclose(archiveFile);
+        fclose(newFile);
+}
+
 int main(int argc, char *argv[])
 {
-	if(argc < 3)
-	{
+	if(argc < 3) {
 		die("Invalid arguments");
 	}
 
-	argv++;
-	char *func_name = *argv;
+	argv++; //argv = 1
+	char *func_name = *argv++; //argv = 2
 	char func_list[] = "list";
 
 	/*
@@ -53,50 +75,66 @@ int main(int argc, char *argv[])
 
 	//From here, must have a password
 	//Must add capability to see if password is given. Currently assuming password is always given
-	argv++;
-	char *pass_option = *argv;
+	char *pass_option = *argv++; //argv = 3
 	char pass_opt[] = "-p\0";
 	char *password;
-	argv++;
 
+        SHA256_CTX ctx;
+        BYTE pass_hash[SHA256_BLOCK_SIZE];
 	// If password option is given
-	if(strcmp(pass_option, pass_opt) == 0)
-	{
-		password = *argv;
+	if(strcmp(pass_option, pass_opt) == 0) {
+		password = *argv++; // argv 3 -> 4
 		checkPassValid(password);
 	} else {
 		password = getpass("Please enter password");
 		checkPassValid(password);
-		argv += 2;
+		argv++; //argv  3 -> 4
 	}
+
+        sha256_init(&ctx);
+        for (int i = 0; i < HASH_ITR; ++idx) {
+                sha256_update(&ctx, password, strlen(password));
+        }
+        sha256_final(&ctx, pass_hash);
+
 
 
 	// Now we have password set and argv is aligned 
 	
 	// Verify if archive file exists. If it does, check pass. Otherwise create
-	argv++;
-	char *file_name = *argv;
-	if(access(file_name, F_OK) != -1)
-	{
-		//encrypt password
-		//check if password is correct
-		//
-	} else {
-		
-		// DO THIS FIRST
-		//open file name in write mode to create file
-		//encrypt password
-		//add to file
-		//close
-		
-		
-		//will have to add file later
+	char *archive_name = *argv++; // argv 4 -> 5
+        char *newFile_name;
+        FILE *archive_fp = NULL;
+        FILE *newFile_fp = NULL;
+        struct stat st;
 
-	
-	
-	}
-		
+        if (stat(archive_name, &st) == 0 && S_ISDIR(st.st_mode)) {
+                die("Not a file");
+        }
+        // NEED TO ADD LOOP FOR MULTIPLE FILES
+        while (newFile_name = *argv++) {
+                if ((strcmp(func_name, "add")) == 0) {
+                        if (access(newFile_name, F_OK) == 0) {
+                                
+                                archive_fp = fopen(archive_name, "ab+");
+                                if (archive_fp == NULL) {
+                                        die("open failed");
+                                }
 
+                                newFile_fp = fopen(newFile_name, "rb");
+                                if(newFile_fp == NULL) {
+                                        die("open failed");
+                                }
+
+                                addFile(newFile_fp, archive_fp); 
+
+	                } else {
+		                
+	                }
+                }
+
+        }
+                
 
 func_end: 
 	return 0;
