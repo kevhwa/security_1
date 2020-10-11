feadcb97bd590a78c8aca364ed6e0dcaeab9d581
Kevin Hwang <jch2169@columbia.edu>
2020-10-10 22:17:50 -0400

    started first server script

---
+++ hw2/server1.sh
@@ -0,0 +1,10 @@
+#~/bin/bash
+
+HOME=/home/mushu/security_1/hw2
+HOMEROOT=$HOME/ca
+HOMEINTER=$HOMEROOT/intermediate
+
+PORT=8000
+DEPTH=4
+
+openssl s_server -accept $PORT -cert $HOMEINTER/certs/www.example_server.com.cert.pem -key $HOMEINTER/private/www.example_server.com.key.pem -pass pass:pass -Verify $DEPTH -CAfile $HOMEROOT/certs/ca.cert.pem -HTTP 
