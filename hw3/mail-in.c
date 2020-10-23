dde9245df48bdaa7c4853a570f6d80ef7a05dc4a
Kevin Hwang <jch2169@columbia.edu>
2020-10-22 21:29:02 -0400

    added mailbox search functionality

--- hw3/mail-in.c
+++ hw3/mail-in.c
@@ -20,6 +20,7 @@ Q
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 #define BUFF_SIZE 4096
 
@@ -27,6 +28,10 @@ static void die(const char *message) {
 	perror(message);
 	exit(1);
 }
+
+int checkEOF(void); 
+int checkValidUser(char *); 
+void skipNext(void);
 
 int main(int argc, char **argv) {
 
@@ -36,7 +41,7 @@ int main(int argc, char **argv) {
 		
 		//Check if EOF
 		int check;
-		if ((check = checkEOF) == 1) {
+		if ((check = checkEOF()) == 1) {
 			printf("EOF REACHED\n");
 			break;
 		}
@@ -55,11 +60,23 @@ int main(int argc, char **argv) {
 
 		if (strcasecmp(method, "mail from") != 0) {
 			fprintf(stderr, "Error in request, skipping\n");
-			//skip to end of email
+			skipNext();
+			continue;
 		} else {
 			printf("Passed mail from check, mail from : %s\n", user);
-			//trim user name;
-			//validate username is in mail system 
+			//trimming name
+
+			printf("length of name: %ld\n", strlen(user));
+			user[strlen(user) - 1] = 0;
+			user[strlen(user) - 1] = 0;
+			user++;
+			printf("trimmed user: %s\n", user);
+
+			if (checkValidUser(user) != 1) {
+				printf("Not valid sender\n");
+				skipNext();
+				continue;
+			}
 		}
 
 		int r_count = 0;
@@ -75,7 +92,8 @@ int main(int argc, char **argv) {
 			if (strcasecmp(requestLine, "data\n") == 0 || strcasecmp(requestLine, "data\r\n") == 0)  {
 				if (r_count == 0) {
 					fprintf(stderr, "DATA out of place, skipping\n");
-					//skip
+					skipNext();
+					break;
 				}
 				printf("Found DATA\n");
 				break;
@@ -87,7 +105,8 @@ int main(int argc, char **argv) {
 
 			if (strcasecmp(method, "rcpt to") != 0) {
 				fprintf(stderr, "Error in request, skipping\n");
-				//skip to end of email
+				skipNext();
+				break;
 			} else {
 				printf("passed rcpt to test, sending to : %s\n", user); 
 				r_count++;
@@ -137,3 +156,72 @@ int checkEOF(void) {
 
 	return 0;
 }
+
+int checkValidUser(char *user) {
+
+	struct dirent *dp;
+	DIR *dfd;
+
+	//*********** need to change when you move exec to bin******
+	if ((dfd = opendir("test/mail")) == NULL) {
+		die("Can't open mail dir\n");
+	}
+
+	//char dir_name[100];
+	//char new_name_qfd[100];
+
+	while ((dp = readdir(dfd)) != NULL)
+	{
+		
+		if (strcmp(user, dp->d_name) == 0) {
+			printf("user found\n");
+			return 1;
+		} else {
+			continue;
+		}
+		
+	}
+
+	return 0;
+}
+
+void skipNext(void) {
+
+	printf("Skipping to next mail\n");
+	char requestLine[1000];
+
+	while (1) {
+	
+		if (fgets(requestLine, sizeof(requestLine), stdin) == NULL) {
+			die("fgets failed\n");	
+		}
+			
+		if (strcmp(requestLine, ".\n" ) == 0 || strcasecmp(requestLine, "data\r\n") == 0 ) {
+			printf("reached end of data\n");
+			return;	
+		}
+	}
+}
+
+// misc code from checking directory name
+//
+//struct stat stbuf;
+//		sprintf(dir_name, "%s"dp->d_name);
+//		if(stat(dir_name, &stbuf ) == -1 ) {
+//			printf("Unable to stat file: %s\n",dir_name) ;
+//			continue ;
+//		}
+
+		//if ((stbuf.st_mode & S_IFMT) == S_IFDIR ) {
+		//	continue;
+			// Skip directories
+		//}
+//		else
+//		{
+//			char* new_name = get_new_name( dp->d_name ) ;// returns the new string
+//			// after removing reqd part
+//			sprintf(new_name_qfd,"%s/%s",dir,new_name) ;
+//			rename( filename_qfd , new_name_qfd ) ;
+
+//		}
+
